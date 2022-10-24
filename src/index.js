5692913c47c411e95d35212c60bb2515d340529e
Александр <102365356+levkin13-08@users.noreply.github.com>
2022-10-24 20:01:22 +0300

    Lesson_1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const myName = 'Alexandr'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
